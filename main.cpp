ce9762b49fe5067316ae6e54c5d37f7c2e420724
Mauricio Landeros <jairo.landeros2885@alumnos.udg.mx>
2020-09-28 10:49:23 -0500

    Prueba01

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cadena;
+    
+    getline(cin, cadena);
+    cout << cadena.size() << endl;
+
+    return;
 
     return 0;
 }
